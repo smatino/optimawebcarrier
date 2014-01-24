a2083fedb6aa3c7a3011827f93024ab5f51d2b7f
Alexander <a.krasnoschek@aimprosoft.com>
2014-01-24 12:32:16 +0200

    add nameGen for DbuRequest.optProgressivo

--- src/main/generated-java/com/optima/opticarrier/web/domain/DbuRequestEditForm.java
+++ src/main/generated-java/com/optima/opticarrier/web/domain/DbuRequestEditForm.java
@@ -2,10 +2,11 @@ package com.optima.opticarrier.web.domain;
 
 import com.optima.opticarrier.domain.DbuRequest;
 import com.optima.opticarrier.repository.DbuRequestRepository;
-import com.optima.opticarrier.util.CsvUtil;
 import com.optima.opticarrier.service.FileContentWriter;
+import com.optima.opticarrier.util.CsvUtil;
 import com.optima.opticarrier.web.domain.support.GenericEditForm;
 import com.optima.opticarrier.web.faces.ConversationContextScoped;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 
 import javax.inject.Inject;
@@ -45,8 +46,16 @@ public class DbuRequestEditForm extends GenericEditForm<DbuRequest, Integer> {
 
     @Override
     protected DbuRequest saveEntity(DbuRequest entity) {
+
+        long time = new Date().getTime();
+
+        if(StringUtils.isBlank(entity.getOptProgressivo())){
+            String optProgressivo = "OPTDBU_" + time;
+            entity.setOptProgressivo(optProgressivo);
+        }
+
         if(toCsv){
-            String fileName = String.format(fileNamePattern, new Date().getTime());
+            String fileName = String.format(fileNamePattern, time);
 
             String[] fields = {"optProgressivo", "dn", "flagPubblicazione", "flagCancellazione", "flagModifica", "ragSociale",
                     "indirizzo", "localita", "provincia", "cap", "codFisc", "pIva", "telefono", "fax", "titoloStudio",
